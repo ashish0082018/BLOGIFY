8dfedefa9ad0734763f44fd307eef99bd58ee736
ashish0082018 <av0082018@gmail.com>
2024-10-21 01:50:11 +0530

    fix error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Createpost from './components/Createpost'
 import Navbar from './components/Navbar'
-import Home from './components/home'
+import Home from './components/Home'
 import Allposts from './components/Allposts'
 import { Route, Routes } from 'react-router-dom'
 import Login from './components/Login'
