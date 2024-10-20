e6bca9156f93eaaaf1d4db0b689076bce58d8182
ashish0082018 <av0082018@gmail.com>
2024-10-21 01:35:55 +0530

    ready for deployment

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,16 @@ import postRoute from "./routes/post.routes.js"
 import userRoute from "./routes/user.routes.js"
 import cors from 'cors'
 import cookieParser from "cookie-parser";
+import path from 'path'
 
 dotenv.config()
 const app=express();
 const PORT=process.env.PORT || 3000;
+
+
+const __dirname=path.resolve();   // get the name of backend directory name
+
+
 // app.use(cookieParser)
 app.use(express.json())
 app.use(cookieParser());
@@ -25,6 +31,13 @@ app.get("/check",(req,res)=>{
 
 app.use("/api/v1/post",postRoute);
 app.use("/api/v1/user",userRoute);
+
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))   // code for serve frontend from backend
+
+app.get("*",(req,res)=>{                                         // routes ke alwa jitna bhi route hoga sab frontend ka hoga
+    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+})
 
 app.listen(PORT,()=>{
     ConnectDB();
